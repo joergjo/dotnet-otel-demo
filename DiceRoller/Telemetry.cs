f54ed8d168c985ab57359bde0b4a8c2d0fad6b0e
Jörg Jooss <joergjo@microsoft.com>
2026-04-13 16:43:35 +0200

    Namespace dice_rolls metric

--- DiceRoller/Telemetry.cs
+++ DiceRoller/Telemetry.cs
@@ -11,5 +11,5 @@ public static class Telemetry
     public const string ServiceNamespace = "dotnet-otel-demo";
     public static readonly ActivitySource DiceRollActivitySource = new(Name);  
     public static readonly Meter DiceMeter = new Meter(Name, "1.0.0");
-    public static readonly Counter<int> DiceRollCounter = DiceMeter.CreateCounter<int>("dice_rolls", description: "Counts the number of dice rolls");
+    public static readonly Counter<int> DiceRollCounter = DiceMeter.CreateCounter<int>("oteldemo.dice_rolls", description: "Counts the number of dice rolls");
 }
